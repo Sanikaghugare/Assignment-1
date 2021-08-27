a12310465a1cdf88ffa958071d349227c4061c44
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-27 12:11:20 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+//Write a program in JAVA to print a welcome text in a separate line. 
+
+  public class Welcome
+  {
+    public static void main( String[] args )
+   {
+    System.out.println("Print a welcome text in a separate line");
+    System.out.println("---------------------------------------");
+    System.out.println( "Welcome to Java Programming!" );
+     } 
+  }

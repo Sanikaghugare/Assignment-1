7e509bd785ba2d41743980d5983f2d952fc9971d
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-27 12:17:54 +0530

    Add files via upload

---
+++ Result_.java
@@ -0,0 +1,20 @@
+/*Write a program in JAVA to print the result of the specified operations.   
+Sample Output:
+Print the result of some specific operation :
+--------------------------------------------------
+Result of 1st expression is : 23
+Result of 2nd expression is : 5
+Result of 3rd expression is : 12
+Result of 4th expression is : 3
+*/
+ 
+public class Result_{
+	public static void main(String[] args) {	  
+	   System.out.println(" Print the result of some specific operation:");	 
+	   System.out.println("---------------------------------------");	  	
+		System.out.println(" Result of 1st expression is : " +( -1+4*6));
+		System.out.println(" Result of 2nd expression is :" + ((35+5)%7));
+		System.out.println(" Result of 3rd expression is:" + (14+-4*6/11));
+		System.out.println(" Result of 4th expression is:" + (2+15/6*1-7%2));
+	}
+}

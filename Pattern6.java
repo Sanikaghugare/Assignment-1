69e6c64af672445b3999d74ccb18e995829551ff
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-29 00:31:10 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,22 @@
+/* Write a program to print the following:
+**********
+**********
+**********
+**********
+*/
+
+class Pattern6
+{
+public static void main(String[] args) 
+{
+ int i,j,k;
+for(i=1; i<=4; i++)
+{
+for(j=1; j<=10; j++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+}

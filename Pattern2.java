44b7f4e596c28191fa61a8e693158781a11707ee
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-29 00:28:38 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,33 @@
+ /*Write a program to print following :
+           *
+         * *
+       * * *
+     * * * *
+   * * * * *
+*/
+   
+import java.io.*;
+public class Pattern2
+{
+    public static void printStars(int n)
+    {
+        int i, j;
+        for(i=0; i<n; i++)
+       {
+            for(j=2*(n-i); j>=0; j--)
+            {
+                System.out.print(" ");
+            }
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        int n = 5;
+        printStars(n);
+    }
+}

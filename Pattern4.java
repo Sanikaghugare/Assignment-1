32df75cdbab504eba4459512c783b93ad470d558
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-29 00:29:56 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,26 @@
+/* Write a program to print the following:
+     1
+   222
+  33333
+ 4444444
+555555555
+*/
+
+public class Pattern4
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 5 - i; j >= 1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}

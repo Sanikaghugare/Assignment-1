b82c7f26e2189f7c39af471d26c80f04c0a5a558
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-29 00:30:36 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,30 @@
+/*Write a program to print the following:
+     1
+   121
+  12321
+ 1234321
+123454321
+*/
+
+public class Pattern5
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 5 - i; j >= 1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}

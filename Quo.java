61ae83008d6d1ba981d79687bffbf654ded952f0
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-29 00:54:40 +0530

    Add files via upload

---
+++ Quo.java
@@ -0,0 +1,17 @@
+/*Write a program in JAVA to divide two numbers and print on the screen.   
+Sample Output:
+Divide two numbers and print:
+----------------------------------
+The quotient of 30 and 10 is : 3
+*/
+
+ public class Quo {
+  public static void main(String[] args) 
+  {
+    int a=30,b=10,quotient;
+    System.out.println("Divide two numbers and print:");
+    System.out.println("---------------------------------");
+    quotient =a/b;
+    System.out.println("The quotient of 30 and 10 is:" +quotient);
+  }
+}

f98344e523b32f73a9487b04774401e2a4e6d3ed
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-08-29 00:14:46 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,11 @@
+//Java Program to Remove all Characters in a String Except Alphabets
+import java.util.Scanner;
+class ABC {
+public static void main(String[] args) {
+     Scanner sc =new Scanner(System.in);
+     System.out.print("Enter String : ");
+     String s = sc.nextLine();
+     s=s.replaceAll("[^a-zA-Z]","");
+     System.out.println(s);
+   }
+}
